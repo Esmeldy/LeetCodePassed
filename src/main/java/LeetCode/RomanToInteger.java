979a63722c0ad468818b05ebbcdd075cfa201c38
Esmeldy Fermin <67498436+Esmeldy@users.noreply.github.com>
2022-10-26 16:12:51 +0200

    Solucionando Ejercicio

--- src/main/java/LeetCode/RomanToInteger.java
+++ src/main/java/LeetCode/RomanToInteger.java
@@ -1,5 +1,9 @@
 package LeetCode;
 
+import java.util.HashMap;
+
+
+
 /**
  * 13. Roman to Integer
  * Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
@@ -8,6 +12,34 @@ package LeetCode;
  */
 public class RomanToInteger {
     public static void main(String[] args) {
+        String num = "IIIX";
+        System.out.println(romanToInt(num));
+    }
 
+    public static <Char> int romanToInt(String s) {
+        char letter = 0;
+        int result = 0;
+        HashMap<Character, Integer> romans = new HashMap<>();
+        romans.put('I',1);
+        romans.put('V',5);
+        romans.put('X',10);
+        romans.put('L',50);
+        romans.put('C',100);
+        romans.put('D',500);
+        romans.put('M',1000);
+
+        if (s.length() < 2 ) result = romans.get(s);
+
+        for (int i=0; i<s.length();i++) {
+                letter = s.charAt(i);
+
+            result += romans.get(letter);
+            //Si el número actual es menor al siguiente se resta
+
+        }
+
+
+
+        return result;
     }
 }
